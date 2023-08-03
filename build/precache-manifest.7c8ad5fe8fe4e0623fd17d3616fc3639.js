1e5e35114c3584267cbe8f6c209aa3e5d5f09a21
Abhinav <abhinav280601@gmail.com>
2023-08-03 15:45:44 +0530

    build

---
+++ build/precache-manifest.7c8ad5fe8fe4e0623fd17d3616fc3639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc677b7c58a6d93fef204a3f4f37d76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73da17616616131466fc",
+    "url": "/static/css/2.15f857eb.chunk.css"
+  },
+  {
+    "revision": "ab2fcebb40563dff59ae",
+    "url": "/static/css/main.a6846ae3.chunk.css"
+  },
+  {
+    "revision": "73da17616616131466fc",
+    "url": "/static/js/2.34aed3e3.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.34aed3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2fcebb40563dff59ae",
+    "url": "/static/js/main.369def90.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
